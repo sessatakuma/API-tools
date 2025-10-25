752c96255ede19207a04772d87088a8462307410
Wei De Chien <wade00754@gmail.com>
2025-10-25 21:28:58 +0800

    feat: add `lifespan` and `get_http_client` fuctions

--- main.py
+++ main.py
@@ -7,12 +7,37 @@ An API interface that provide two functionalities
 (5) Sentence Query  (/api/SentenceQuery/)
 """
 
+from contextlib import asynccontextmanager
+
+import httpx
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from api import accent_marker, dict_query, furigana_marker, sentence_query, usage_query
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Lifespan context manager for FastAPI application.
+
+    This function is used to manage the lifespan of the FastAPI application.
+    It can be used to set up resources before the application starts and
+    clean up resources after the application stops.
+
+    Args:
+        app (FastAPI): The FastAPI application instance.
+    """
+    # Set up resources before the application starts
+    app.state.http_client = httpx.AsyncClient()
+    yield
+    # Clean up resources after the application stops
+    await app.state.http_client.aclose()
+
+
+app = FastAPI(lifespan=lifespan)
+
+# Configure CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
