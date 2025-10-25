7856b9e7f880c35795b437bac8fd093a4f845c36
Wei De Chien <wade00754@gmail.com>
2025-10-25 20:56:16 +0800

    refactor(main): add doc strings and format with ruff

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 An API interface that provide two functionalities
 (1) Accent Marker   (/api/MarkAccent/)
 (2) Furigana Marker (/api/MarkFurigana/)
+(3) Usage Query    (/api/UsageQuery/)
+(4) Dictionary Query (/api/DictQuery/)
+(5) Sentence Query  (/api/SentenceQuery/)
 """
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import accent_marker, furigana_marker, usage_query, dict_query, sentence_query
+
+from api import accent_marker, dict_query, furigana_marker, sentence_query, usage_query
 
 app = FastAPI()
 app.add_middleware(
@@ -20,7 +24,6 @@ app.add_middleware(
 # Include routers from different modules
 app.include_router(accent_marker.router, prefix="/api")
 app.include_router(furigana_marker.router, prefix="/api")
-app.include_router(usage_query.router, prefix='/api')
-app.include_router(dict_query.router, prefix='/api')
-app.include_router(sentence_query.router, prefix='/api')
-    
+app.include_router(usage_query.router, prefix="/api")
+app.include_router(dict_query.router, prefix="/api")
+app.include_router(sentence_query.router, prefix="/api")
