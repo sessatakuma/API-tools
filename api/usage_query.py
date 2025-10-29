be40e68f7bd1a93c733806bcad20645f00234b89
Wei De Chien <72686306+wade00754@users.noreply.github.com>
2025-10-30 00:28:57 +0800

    fix(usage_query)!: fix wrong return type and wrong return data (#26)
    * fix(usage_query): fix the wrong type hint
    
    * fix(usage_query): fix wrong return data if it is an empty list
    
    * refactor(usage_query)!: update comments to show the correct info for the APIs
    
    BREAKING CHANGE: the request parameter for `/api/UsageQuery/IdDetails/`
    changes from `id` to `headword_id`.

--- api/usage_query.py
+++ api/usage_query.py
@@ -26,9 +26,9 @@ class HeadWordRequest(BaseModel):
 
 
 class IdRequest(BaseModel):
-    """Class representing a request object for ID"""
+    """Class representing a request object for headword ID"""
 
-    id: str = Field(description="The ID of the word")
+    headword_id: str = Field(description="The headword ID of the word")
     site: Literal["NLB", "NLT"] = Field(
         default="NLB",
         description="The site to query, either 'NLB' or 'NLT'. Default is 'NLB'.",
@@ -65,17 +65,17 @@ class URL(BaseModel):
 class IdDetails(BaseModel):
     """Class representing details of a word"""
 
-    base: dict[str, str] = Field(description="The base form of the word")
-    subcorpus: list[dict[str, str]] = Field(description="The subcorpus of the word")
-    shojikei: list[dict[str, str]] = Field(description="The shojikei of the word")
-    subcorpus_shojikei: list[dict[str, str]] = Field(
+    base: dict[str, Any] = Field(description="The base form of the word")
+    subcorpus: list[dict[str, Any]] = Field(description="The subcorpus of the word")
+    shojikei: list[dict[str, Any]] = Field(description="The shojikei of the word")
+    subcorpus_shojikei: list[dict[str, Any]] = Field(
         description="The distribution of shojikei by subcorpus of the word"
     )
-    katuyokei: list[dict[str, str]] = Field(description="The katuyokei of the word")
-    setuzoku: list[dict[str, str]] = Field(
+    katuyokei: list[dict[str, Any]] = Field(description="The katuyokei of the word")
+    setuzoku: list[dict[str, Any]] = Field(
         description="The subsequent auxiliary verbs of the word"
     )
-    patternfreqorder: list[dict[str, str]] = Field(
+    patternfreqorder: list[dict[str, Any]] = Field(
         description="The frequency of the word in different patterns"
     )
 
@@ -117,7 +117,7 @@ class IdResponse(BaseModel):
         default=200, description="Status code of response align with RFC 9110"
     )
     result: Optional[IdDetails] = Field(
-        description="Details of the word with the given ID"
+        description="Details of the word with the given headword ID"
     )
     error: Optional[ErrorInfo] = Field(
         default=None,
@@ -150,7 +150,7 @@ def text_type(text: str) -> str | None:
 async def get_headwords(
     request: HeadWordRequest, client: httpx.AsyncClient = Depends(get_http_client)
 ) -> dict[str, Any]:
-    """Get headword_list for the word."""
+    """Get the lists of information of headwords with the given word."""
 
     match text_type(request.word):
         case "yomi":
@@ -201,7 +201,7 @@ async def get_headwords(
         try:
             data = response.json()
             result = []
-            if data.get("rows") and len(data["rows"]) > 0:
+            if data.get("rows") is not None and len(data["rows"]) > 0:
                 for row in data["rows"]:
                     id = row.get("id")
                     headword_id = row.get("headword_id")
@@ -259,7 +259,7 @@ async def get_headwords(
 async def get_urls(
     request: HeadWordRequest, client: httpx.AsyncClient = Depends(get_http_client)
 ) -> dict[str, Any]:
-    """Get URL for the word with the given word."""
+    """Get the URLs of words with the given word."""
     response = await get_headwords(request, client)
 
     if response["status"] != 200:
@@ -286,7 +286,7 @@ async def get_urls(
 async def get_id_details(
     request: IdRequest, client: httpx.AsyncClient = Depends(get_http_client)
 ) -> dict[str, Any]:
-    """Get details for the word with the given ID."""
+    """Get the details of the given headword ID."""
 
     async def fetch_data(
         mode: Literal["get", "post"], endpoint: str, target: str = ""
@@ -295,12 +295,12 @@ async def get_id_details(
         match mode:
             case "get":
                 headers = {
-                    "Referer": f"{SITE[request.site]}/headword/{request.id}/",
+                    "Referer": f"{SITE[request.site]}/headword/{request.headword_id}/",
                     "User-Agent": "Mozilla/5.0",
                 }
                 try:
                     response = await client.get(
-                        f"{SITE[request.site]}/{endpoint}/{request.id}/",
+                        f"{SITE[request.site]}/{endpoint}/{request.headword_id}/",
                         headers=headers,
                     )
                 except httpx.TimeoutException:
@@ -318,12 +318,12 @@ async def get_id_details(
             case "post":
                 headers = {
                     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                    "Referer": f"{SITE[request.site]}/headword/{request.id}/",
+                    "Referer": f"{SITE[request.site]}/headword/{request.headword_id}/",
                     "User-Agent": "Mozilla/5.0",
                 }
                 try:
                     response = await client.post(
-                        f"{SITE[request.site]}/{endpoint}/{request.id}/",
+                        f"{SITE[request.site]}/{endpoint}/{request.headword_id}/",
                         headers=headers,
                     )
                 except httpx.TimeoutException:
@@ -342,7 +342,7 @@ async def get_id_details(
         if response.status_code == 200:
             try:
                 data = response.json()
-                ret = data[target] if target and data.get(target) else data
+                ret = data[target] if target and data.get(target) is not None else data
                 # print(f"Fetched {target}: {ret}")
                 return ret
             except Exception as e:
